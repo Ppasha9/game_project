223f0ad09b4ec5cb9cbb78b8f585254b7e843f38
Pavel Denisov(PD6) <pavelvan50@gmail.com>
2018-03-04 19:57:00 +0300

    First Global Commit
    Create project and add 'main.cpp' file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  MessageBoxA(NULL, "Top Game Development :-)", "Game App", 0);
+  return 0;
+}
+
+// END OF 'main.cpp' FILE
